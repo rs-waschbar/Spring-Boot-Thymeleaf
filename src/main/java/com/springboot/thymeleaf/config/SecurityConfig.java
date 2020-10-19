a93725f5a7c1167aacc8faf16c3ee204ee1c54a3
Ruslan <timbaraccoon@gmail.com>
2020-10-19 23:15:46 +0500

    ...

---
+++ src/main/java/com/springboot/thymeleaf/config/SecurityConfig.java
@@ -0,0 +1,29 @@
+package com.springboot.thymeleaf.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+import javax.sql.DataSource;
+
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Qualifier("securityDataSource")
+    private final DataSource securityDataSource;
+
+    @Autowired
+    public SecurityConfig(DataSource securityDataSource) {
+        this.securityDataSource = securityDataSource;
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.jdbcAuthentication().dataSource(securityDataSource);
+    }
+}
